06248f8de60cfa5141d409f1b7e93e39eb200ff1
Patrick <patrick.aba@gmail.com>
2020-09-14 14:14:21 +1000

    install gsap and support for code transpilation of specific node_modules via next-transpile-modules 2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
-const withTM = require('next-transpile-modules')(['gsap', 'gsap/ScrollTrigger']); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')(['gsap/ScrollTrigger']); // pass the modules you would like to see transpiled
  
 module.exports = withTM();
