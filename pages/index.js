14f9254daeb840ed1da9979a6f49b68237f81b40
Patrick <patrick.aba@gmail.com>
2020-09-14 21:00:58 +1000

    Fixes SSR issue (where gsap is client side while nextJS is SSR) via useEffect to wait until component is loaded to look for the document. https://greensock.com/forums/topic/19160-react-nextjs-and-gsap-issues-possible-fixes-for-ssr/\?do\=findComment\&comment\=91115

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,33 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
 import React, { useEffect } from 'react'
 
 import styles from '../styles/Home.module.css'
 
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// gsap.registerPlugin(ScrollTrigger);
-
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('./svg'),
-  { ssr: false }
-)
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   // ScrollMagic === ScrollTrigger
-  // const tl = gsap.timeline()
-  // tl.from("#room", { scale: 5 })
-  
-  // ScrollTrigger.defaults({
-  //   toggleActions: "restart pause resume none",
-  //   markers: true,
-  // })
 
-  // ScrollTrigger.create({
-  //   animation: tl,
-  //   trigger: ".container",
-  //   start: 300,
-  //   end: 600,
-  // })
+  useEffect(() => {
+    // Update the document title using the browser API
+    const tl = gsap.timeline()
+    tl.from("#room", { scale: 5 })
+    
+    ScrollTrigger.defaults({
+      toggleActions: "restart pause resume none",
+      markers: true,
+    })
+
+    ScrollTrigger.create({
+      animation: tl,
+      trigger: ".container",
+      start: 300,
+      end: 600,
+    })
+  });
   return (
     <div className={styles.container}>
       <Head>
@@ -37,7 +35,53 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <DynamicComponentWithNoSSR />
+      <main className={styles.main}>
+        <div className={styles.svgs}>
+          {/* <img id="foreground" src="/foreground.svg" alt="Foreground" className={styles.homeFg} /> fg is to thhe right of mid, in front of bg */}
+          {/* <img id="mid" src="/mid.svg" alt="Mid" className={styles.homeMid} /> mid is in front of the apartment bg */}
+          {/* <img id="background" src="/background.svg" alt="Background" className={styles.homeBg} /> bg is the apartment that room is in */}
+          <img id="room" src="/room.svg" alt="Room" className={styles.homeRoom} />
+        </div>
+          {/* <img src="/Background.svg" alt="Vercel Logo" className={styles.logo} /> */}
+        {/* <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div> */}
+      </main>
 
       <footer className={styles.footer}>
         <a
